da5cb502bd410682c48b662689f80fce88fa3d13
PHUC NGUYEN <phuccrv@gmail.com>
2023-08-17 10:47:08 +0700

    upcode

---
+++ src/data/data.ts
@@ -0,0 +1,30 @@
+ 
+ export const dataContent =  [
+    {
+    id: 1,
+    postTitle: 'The Art of Makeup: Creating the Perfect Look',
+    postCategory: 'Beauty',
+    author: 'Beauty Guru',
+    date: '6/30/2016',
+    postSnippet: 'Makeup can transform your appearance...',
+    imageSrc: '../images-content1.jpg',
+  },
+  {
+    id: 2,
+    postTitle: 'The Art of Makeup: Creating the Perfect Look',
+    postCategory: 'Beauty',
+    author: 'Blasd Guru',
+    date: '7/20/2300',
+    postSnippet: 'Makeup can transform your appearance...',
+    imageSrc: '../images-content2.jpg',
+  },
+  {
+    id: 3,
+    postTitle: 'The Art of Makeup: Creating the Perfect Look',
+    postCategory: 'Beauty',
+    author: 'Beauty daga',
+    date: '8/40/2015',
+    postSnippet: 'Makeup can transform your appearance...',
+    imageSrc: '../images-content3.jpg',
+  },
+]
