489221af85a0824ee9a133d2dbd006e4a0271777
PHUC NGUYEN <phuccrv@gmail.com>
2023-08-18 15:55:19 +0700

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,30 +1,43 @@
- 
- export const dataContent =  [
-    {
+export const dataContent = [
+  {
     id: 1,
-    postTitle: 'The Art of Makeup: Creating the Perfect Look',
+    postTitle: 'Looking Better In Photos Based On The Body Type',
     postCategory: 'Beauty',
     author: 'Beauty Guru',
     date: '6/30/2016',
-    postSnippet: 'Makeup can transform your appearance...',
+    postSnippet:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bo…',
     imageSrc: '../images-content1.jpg',
+    infor:'17 Books You Should Read Before (or at) University'
   },
   {
     id: 2,
-    postTitle: 'The Art of Makeup: Creating the Perfect Look',
-    postCategory: 'Beauty',
-    author: 'Blasd Guru',
-    date: '7/20/2300',
-    postSnippet: 'Makeup can transform your appearance...',
+    postTitle: '17 Books You Should Read Before (Or At) University',
+    postCategory: 'Fashion',
+    author: 'Sora Blogging Tips',
+    date: '3/17/2016',
+    postSnippet:
+      'From inspiring novels to frank discussions of sexuality, these are the books students wish they had read to ease the transition to university and prepare for a new stage in their academic and personal lives.',
     imageSrc: '../images-content2.jpg',
   },
   {
     id: 3,
-    postTitle: 'The Art of Makeup: Creating the Perfect Look',
-    postCategory: 'Beauty',
-    author: 'Beauty daga',
-    date: '8/40/2015',
-    postSnippet: 'Makeup can transform your appearance...',
+    postTitle: '25 Top Tourist Attractions In The USA',
+    postCategory: 'People',
+    author: 'Sora Blogging Tips',
+    date: '3/17/2016',
+    postSnippet:
+      'What makes you want to dance with joy? What puts the spring in your sandals, the Gangnam Style in your golf shoes, the hip hop in your high tops? Whatever it happens to be, we’re willing to bet we can serve it up to you in extra helpings here in Birmingham.',
     imageSrc: '../images-content3.jpg',
   },
-]
+  {
+    id: 4,
+    postTitle: '30 Top Tourist Attractions In The VietNam',
+    postCategory: 'People',
+    author: 'Sora Blogging Tips',
+    date: '38/18/2023',
+    postSnippet:
+      'Misty mountains, cascading rice terraces, thick jungle, unspoiled beaches and two of Southeast Asia’s most time-honoured cities – the variety of landscapes in Vietnam seems so endless, it’s hard to know where to begin. For most visitors, it all starts – or ends – in Hanoi, the chaotic capital of the north.',
+    imageSrc: '../images-content4.jpg',
+  },
+];
