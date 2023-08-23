043c6be424818812e84b7211fb326211a6dae896
PHUC NGUYEN <phuccrv@gmail.com>
2023-08-23 14:23:44 +0700

    update gaio dien

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,43 +1,81 @@
 export const dataContent = [
   {
     id: 1,
-    postTitle: 'Looking Better In Photos Based On The Body Type',
-    postCategory: 'Beauty',
-    author: 'Beauty Guru',
-    date: '6/30/2016',
+    postTitle: 'PT. META TECHNOLOGY INDONESIA was established in 2003',
+    postCategory: 'IT',
+    author: 'Consulting',
+    date: '6/30/2003',
     postSnippet:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bo…',
-    imageSrc: '../images-content1.jpg',
+      'From the beginning we are constantly focus in web based application solution. Since then we have a lot of product for core operation system for various line of business and already success implemented in a lot of big national and multinational company in Indonesia and overseas.',
+    imageSrc: 'https://hinh365.com/wp-content/uploads/2020/06/53c7add708e23a918b9fed1084f302cf.jpg',
+    infor:'17 Books You Should Read Before (or at) University',
+    numberPage: 2
+  },
+  {
+    id: 2,
+    postTitle: 'META AOS (Automotive Operating System) / Dealer Management System',
+    postCategory: 'Meta',
+    author: 'Account Management',
+    date: '3/17/2003',
+    postSnippet:
+      'From inspiring novels to frank discussions of sexuality, these are the books students wish they had read to ease the transition to university and prepare for a new stage in their academic and personal lives.',
+    imageSrc: 'https://img.lovepik.com/photo/50073/5109.jpg_wh860.jpg',
+    numberPage: 1
+  },
+  {
+    id: 3,
+    postTitle: 'META CRM (Customer Relationship Management)',
+    postCategory: 'META',
+    author: 'CRM',
+    date: '3/17/2013',
+    postSnippet:
+      'META CRM (Customer Relationship Management) is a web based application to help your company reaching the goal. This application have feature for helping your company to maintain a good relationship with your customer.',
+    imageSrc: 'https://thuthuatnhanh.com/wp-content/uploads/2020/09/tai-anh-nen-cong-nghe-cho-pc.jpg',
+    numberPage: 3
+  },
+  {
+    id: 4,
+    postTitle: 'META CTS (Cloud Trading System)',
+    postCategory: 'Application',
+    author: 'sales',
+    date: '38/18/2003',
+    postSnippet:
+      'This application accommodate the whole transactions and activities in the trading line of business, such as : sales, purchasing,  Inventory, re-assembling ',
+    imageSrc: 'https://img4.thuthuatphanmem.vn/uploads/2020/07/05/anh-background-cong-nghe-may-tinh_035952957.jpg',
+    numberPage: 1
+  },
+];
+
+export const dataFooter = [
+  {
+    id: 1,
+    postTitle: 'META TECHNOLOGY INDONESIA was established in 2003',
+    postCategory: 'IT',
+    author: 'Consulting',
+    date: '6/30/2003',
+    postSnippet:
+      'From the beginning we are constantly focus in web based application solution. Since then we have a lot of product for core operation system for various line of business and already success implemented in a lot of big national and multinational company in Indonesia and overseas.',
+    imageSrc: 'https://hinh365.com/wp-content/uploads/2020/06/53c7add708e23a918b9fed1084f302cf.jpg',
     infor:'17 Books You Should Read Before (or at) University'
   },
   {
     id: 2,
-    postTitle: '17 Books You Should Read Before (Or At) University',
-    postCategory: 'Fashion',
-    author: 'Sora Blogging Tips',
-    date: '3/17/2016',
+    postTitle: 'META AOS (Automotive Operating System)',
+    postCategory: 'Meta',
+    author: 'Account Management',
+    date: '3/17/2003',
     postSnippet:
       'From inspiring novels to frank discussions of sexuality, these are the books students wish they had read to ease the transition to university and prepare for a new stage in their academic and personal lives.',
-    imageSrc: '../images-content2.jpg',
+    imageSrc: 'https://img.lovepik.com/photo/50073/5109.jpg_wh860.jpg',
   },
   {
     id: 3,
-    postTitle: '25 Top Tourist Attractions In The USA',
-    postCategory: 'People',
-    author: 'Sora Blogging Tips',
-    date: '3/17/2016',
+    postTitle: 'META CRM (Customer Relationship Management)',
+    postCategory: 'META',
+    author: 'CRM',
+    date: '3/17/2013',
     postSnippet:
-      'What makes you want to dance with joy? What puts the spring in your sandals, the Gangnam Style in your golf shoes, the hip hop in your high tops? Whatever it happens to be, we’re willing to bet we can serve it up to you in extra helpings here in Birmingham.',
-    imageSrc: '../images-content3.jpg',
+      'META CRM (Customer Relationship Management) is a web based application to help your company reaching the goal. This application have feature for helping your company to maintain a good relationship with your customer.',
+    imageSrc: 'https://thuthuatnhanh.com/wp-content/uploads/2020/09/tai-anh-nen-cong-nghe-cho-pc.jpg',
   },
-  {
-    id: 4,
-    postTitle: '30 Top Tourist Attractions In The VietNam',
-    postCategory: 'People',
-    author: 'Sora Blogging Tips',
-    date: '38/18/2023',
-    postSnippet:
-      'Misty mountains, cascading rice terraces, thick jungle, unspoiled beaches and two of Southeast Asia’s most time-honoured cities – the variety of landscapes in Vietnam seems so endless, it’s hard to know where to begin. For most visitors, it all starts – or ends – in Hanoi, the chaotic capital of the north.',
-    imageSrc: '../images-content4.jpg',
-  },
-];
+]
